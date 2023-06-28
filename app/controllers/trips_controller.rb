40b83fea66a92cad0c8c14511c5329c032e976ed
frankie kapp <geos_tricks@outlook.com>
2023-06-28 22:57:47 +0800

    configure pundit for trips

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,20 +2,23 @@ class TripsController < ApplicationController
   before_action :set_trip, only: [:show, :edit, :update, :destroy]
 
   def index
-    @trips = Trip.all
+    @trips = policy_scope(Trip)
   end
 
   def show
+    # authorize @trip
   end
 
   def new
     @trip = Trip.new
+    # authorize @trip
   end
 
   def create
     # @user = User.find(params[:user_id])
     @trip = Trip.new(trip_params)
     @trip.user = current_user
+    # authorize @trip
     if @trip.save
       redirect_to @trip, notice: "Trip was successfully created."
     else
@@ -24,9 +27,11 @@ class TripsController < ApplicationController
   end
 
   def edit
+    # authorize @trip
   end
 
   def update
+    # authorize @trip
     if @trip.update(trip_params)
       redirect_to @trip, notice: "Trip was successfully updated."
     else
@@ -35,6 +40,7 @@ class TripsController < ApplicationController
   end
 
   def destroy
+    # authorize @trip
     @trip.destroy
     redirect_to trips_path, notice: "Trip was successfully destroyed."
   end
@@ -43,9 +49,10 @@ class TripsController < ApplicationController
 
   def set_trip
     @trip = Trip.find(params[:id])
+    authorize @trip
   end
 
   def trip_params
-    params.require(:trip).permit(:destination, :theme, :date, :user_id)
+    params.require(:trip).permit(:destination, :theme, :date)
   end
 end
